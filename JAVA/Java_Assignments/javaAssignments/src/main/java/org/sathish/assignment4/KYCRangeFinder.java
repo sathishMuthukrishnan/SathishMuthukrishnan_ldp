fa023c833c315753a8a2cc25e2ccaf1d97aeca80
sathish muthukrishnan <sathish.muthukrishnan@zemosolabs.com>
2025-05-23 12:15:37 +0530

    I have made the suggested changes

--- JAVA/Java_Assignments/javaAssignments/src/main/java/org/sathish/assignment4/KYCRangeFinder.java
+++ JAVA/Java_Assignments/javaAssignments/src/main/java/org/sathish/assignment4/KYCRangeFinder.java
@@ -1,3 +1,5 @@
+package org.sathish.assignment4;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
@@ -16,26 +18,34 @@ public class KYCRangeFinder {
             String result = findKYCWindow(input[0], input[1]);
             System.out.println(result);
         }
+        // Closing scanner to prevent resource leak
+        scanner.close();
 
 }
+    /**
+     * This method calculates the KYC window based on signup date and current date.
+     * It returns the start and end dates of the KYC update window or "No range" if invalid.
+     */
 public static String findKYCWindow(String signupStr, String currentStr) {
     try {
         LocalDate signupDate = LocalDate.parse(signupStr, formatter);
         LocalDate currentDate = LocalDate.parse(currentStr, formatter);
 
+        // If signup date is after current date, return no range
         if (signupDate.isAfter(currentDate)) {
             return "No range";
         }
-
+        // Set anniversary to current year
         LocalDate anniversary = signupDate.withYear(currentDate.getYear());
-
+        // If anniversary is after current date, consider previous year
         if (anniversary.isAfter(currentDate)) {
             anniversary = anniversary.minusYears(1);
         }
-
+        // 30 days before and after the anniversary date
         LocalDate startWindow = anniversary.minusDays(30);
         LocalDate endWindow = anniversary.plusDays(30);
 
+        // Adjust end window if it exceeds the current date
         if (endWindow.isAfter(currentDate)) {
             endWindow = currentDate;
         }
